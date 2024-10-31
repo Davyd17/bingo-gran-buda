aa31ec1e229030a079dc853e812ccd003830067d
David <davidcq55@gmail.com>
2024-10-30 23:11:33 -0500

    Initial commit
        - Setup project structure
        - Add .gitignore file
        - Add basic project files

---
+++ src/main/java/com/bingogranbuda/bingo/BingoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bingogranbuda.bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bingogranbuda/bingo/BingoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingogranbuda.bingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
