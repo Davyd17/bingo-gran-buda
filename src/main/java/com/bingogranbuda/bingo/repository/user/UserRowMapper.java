656f1edc8656eb93b0bf7c66d7e5e0812d058284
David <davidcq55@gmail.com>
2024-11-04 02:19:50 -0500

    feat: implement User repository classes for user handling
    - Introduced UserDao interface to specify data access operations for user entities
    - Developed UserDaoImpl class to perform database actions related to users
    - Created UserRowMapper class to convert user data from ResultSet to User objects

---
+++ src/main/java/com/bingogranbuda/bingo/repository/user/UserRowMapper.java
@@ -0,0 +1,20 @@
+package com.bingogranbuda.bingo.repository.user;
+
+import com.bingogranbuda.bingo.model.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new User(
+                rs.getInt("id"),
+                rs.getString("username"),
+                rs.getString("password"),
+                rs.getTimestamp("created_at")
+        );
+    }
+}
