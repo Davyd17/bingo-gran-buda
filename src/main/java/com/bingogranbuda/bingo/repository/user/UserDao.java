de0bec1a426876e9f708b619c3725fc887482cf2
David <davidcq55@gmail.com>
2024-11-04 02:19:50 -0500

    feat: implement User repository classes for user handling
    - Introduced UserDao interface to specify data access operations for user entities
    - Developed UserDaoImpl class to perform database actions related to users
    - Created UserRowMapper class to convert user data from ResultSet to User objects

---
+++ src/main/java/com/bingogranbuda/bingo/repository/user/UserDao.java
@@ -0,0 +1,7 @@
+package com.bingogranbuda.bingo.repository.user;
+
+import com.bingogranbuda.bingo.model.User;
+import com.bingogranbuda.bingo.repository.Dao;
+
+public interface UserDao extends Dao<User> {
+}
