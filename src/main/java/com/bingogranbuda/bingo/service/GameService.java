d7db4e78d6e04150e1ca2cab70a812ed242a723a
David <davidcq55@gmail.com>
2025-01-10 11:14:56 -0500

    fix: Added missing @Service annotation to GameService

--- src/main/java/com/bingogranbuda/bingo/service/GameService.java
+++ src/main/java/com/bingogranbuda/bingo/service/GameService.java
@@ -2,7 +2,9 @@ package com.bingogranbuda.bingo.service;
 
 import com.bingogranbuda.bingo.model.Game;
 import com.bingogranbuda.bingo.repository.game.GameDao;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GameService extends AbstractService<Game>{
 
     public GameService(GameDao gameDao) {
